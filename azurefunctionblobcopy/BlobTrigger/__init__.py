d83acbf5b2f6a6eac56cd474af339d6a5e42f06b
alfonsof <alfonsofb@gmail.com>
2021-08-16 16:41:57 +0200

    Refactoring

--- azurefunctionblobcopy/BlobTrigger/__init__.py
+++ azurefunctionblobcopy/BlobTrigger/__init__.py
@@ -5,10 +5,11 @@ import azure.functions as func
 
 def main(inputblob: func.InputStream, outputblob: func.Out[bytes]):
     logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {inputblob.name}\n"
+                 f"Blob Name: {inputblob.name}\n"
                  f"Blob Size: {inputblob.length} bytes\n"
-                 f"Full Blob URI: {inputblob.uri}")
+                 f"Blob URI: {inputblob.uri}")
     readbytes = inputblob.read()
     logging.info(f'Trigger function processed {len(readbytes)} bytes')
+    # Copy blob to output
     outputblob.set(readbytes)
     logging.info(f"Copied")
